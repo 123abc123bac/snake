2f06caaeaadaf94d4a79c3a842c23de8a54dcba8
Eckt Li <103355039+123abc123bac@users.noreply.github.com>
2024-06-29 14:47:55 +0800

    Add files via upload

---
+++ game.js
@@ -0,0 +1,52 @@
+class Game{
+    constructor(select, scoreEle, gameoverbg, btnst){
+        this.btnstart = document.querySelector(btnst)
+        this.gameoverimg = document.querySelector(gameoverbg)
+        this.map = document.querySelector(select)
+        this.scoreEle = document.querySelector(scoreEle)
+        this.food = new Food(select)
+
+        this.snake = new Snake(select)
+        this.timer = 0
+        this.count = 0
+    }
+    start(){
+        this.gameoverimg.style.display = "none"
+        this.timer = setInterval(() => {
+            this.snake.move()
+            if(this.snake.isEat(this.food.x, this.food.y)){
+                this.snake.createHead()
+                this.food.foodPosition()
+                this.scoreAdd()
+            }
+
+            if(this.snake.isDie()){
+                clearInterval(this.timer)
+                this.gameover()
+            }
+        }, 100);
+    }
+
+    pause(){
+        clearInterval(this.timer)
+    }
+
+    restart(){
+        this.btnstart.disabled = false
+        this.gameoverimg.style.display = "none"
+        window.location.reload()
+    }
+
+    change(type){
+        this.snake.direction = type
+    }
+    scoreAdd()
+    {
+        this.count++
+        this.scoreEle.innerText = this.count
+    }
+    gameover(){
+        this.gameoverimg.style.display = "block"
+        this.btnstart.disabled = true
+    }
+}
